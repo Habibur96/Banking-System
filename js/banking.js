c86b2f861171599167614138f059dd1c4abee3b5
Habibur96 <habiburrahmannayan66@gmail.com>
2022-04-08 12:42:59 +0600

    balance update

--- js/banking.js
+++ js/banking.js
@@ -3,24 +3,32 @@
 // handle deposit button event
 
 document.getElementById('deposit-button').addEventListener('click', function () {
+
+    //get the amount deposited
     const a = document.getElementById('click-deposit')
 
     const c = a.value;
+    const num2 = parseFloat(c);
     a.value = '';
 
+    //update deposit total
     const b = document.getElementById('dolar');
-    // const d = b + c;
-    b.innerText = c;
+    const e = b.innerText;
+    const num1 = parseFloat(e);
 
-    // const d = b.innerText;
+    const d = num1 + num2;
+
+    b.innerText = d;
+
+    // update account balance
+
+    const q = document.getElementById('balance');
+    const z = q.innerText;
+    const num3 = parseFloat(z);
+    const f = num2 + num3;
 
 
-
-    b = final;
-
-
-
-
+    q.innerText = f;
 
 
 
