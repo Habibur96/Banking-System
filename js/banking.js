bf918cdd5cf3c8a0986c7edc60b744010b028af4
Habibur96 <habiburrahmannayan66@gmail.com>
2022-04-08 10:26:20 +0600

    hande deposit

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,27 @@
+
+
 // handle deposit button event
+
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const a = document.getElementById('click-deposit')
+
+    const c = a.value;
+    a.value = '';
+
+    const b = document.getElementById('dolar');
+    // const d = b + c;
+    b.innerText = c;
+
+    // const d = b.innerText;
+
+
+
+    b = final;
+
+
+
+
+
+
+
+})
